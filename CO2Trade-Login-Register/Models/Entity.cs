ce58c50b4c811404d37aed1d776f17ae7be07823
JonaCaucota <jonacau12@gmail.com>
2023-09-26 21:43:06 -0300

    added Entity

---
+++ CO2Trade-Login-Register/Models/Entity.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace CO2Trade_Login_Register.Models;
+
+public class Entity : IdentityUser
+{
+    public string BusinessName { get; set; }
+    public string Address { get; set; }
+    public int EntityType { get; set; }
+    public string Description { get; set; }
+    public string PhoneNumber { get; set; }
+}
