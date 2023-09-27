a53fdfb7c79eadc714cde62e18699d48d4e34b6b
JonaCaucota <jonacau12@gmail.com>
2023-09-26 21:50:15 -0300

    added db context in Program.cs

--- CO2Trade-Login-Register/Program.cs
+++ CO2Trade-Login-Register/Program.cs
@@ -1,6 +1,19 @@
+using CO2Trade_Login_Register.Data;
+using CO2Trade_Login_Register.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//Database
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
+});
+
+builder.Services.AddIdentity<EntityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
