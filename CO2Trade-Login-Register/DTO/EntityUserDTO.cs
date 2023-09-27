4a598d8bd821b8e6dfdf854dee7f3dcf5ae8b058
JonaCaucota <jonacau12@gmail.com>
2023-09-27 14:59:10 -0300

    fix username in response

--- CO2Trade-Login-Register/DTO/EntityUserDTO.cs
+++ CO2Trade-Login-Register/DTO/EntityUserDTO.cs
@@ -4,5 +4,5 @@ public class EntityUserDTO
 {
     public string ID { get; set; }
     public string UserName { get; set; }
-    public string Name { get; set; }
+    public string BusinessName { get; set; }
 }
