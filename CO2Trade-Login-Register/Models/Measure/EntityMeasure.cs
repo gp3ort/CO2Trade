3f0d7b3101ef0c29af338dbf9b4811b920e5e65a
JonaCaucota <jonacau12@gmail.com>
2023-09-27 21:13:50 -0300

    Added new entitie MeasureCO2

--- CO2Trade-Login-Register/Models/Measure/EntityMeasure.cs
+++
@@ -1,9 +0,0 @@
-namespace CO2Trade_Login_Register.Models.Measure;
-
-public class EntityMeasure
-{
-    public int Id;
-    public int IdEntity;
-    public int IdMeasureCO2;
-
-}
--- CO2Trade-Login-Register/Models/Measure/MesaureCO2.cs
+++
@@ -1,6 +0,0 @@
-namespace CO2Trade_Login_Register.Models.Measure;
-
-public class MesaureCO2
-{
-    
-}
