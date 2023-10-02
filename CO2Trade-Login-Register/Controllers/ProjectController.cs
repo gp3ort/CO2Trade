8bdd68b44cd616d8cf366aba5626b99771cb8bbf
JonaCaucota <jonacau12@gmail.com>
2023-10-02 18:34:45 -0300

    Added ProjectService ProjectRepository and interfaces

---
+++ CO2Trade-Login-Register/Controllers/ProjectController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CO2Trade_Login_Register.Controllers;
+
+[Route("api/Project")]
+[ApiController]
+public class ProjectController : ControllerBase
+{
+    
+}
---
+++ CO2Trade-Login-Register/Service/ProjectService.cs
@@ -0,0 +1,8 @@
+using CO2Trade_Login_Register.Service.IService;
+
+namespace CO2Trade_Login_Register.Service;
+
+public class ProjectService : IProjectService
+{
+    
+}
