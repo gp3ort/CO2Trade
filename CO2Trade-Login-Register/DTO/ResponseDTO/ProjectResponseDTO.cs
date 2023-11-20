a0d1c796ff07b6594b928765b2724eeb730e5f6d
Lucas Martin Funes <38304769@comunidadort.edu.ar>
2023-11-20 19:23:31 -0300

    Tons added to response

--- CO2Trade-Login-Register/DTO/ResponseDTO/ProjectResponseDTO.cs
+++ CO2Trade-Login-Register/DTO/ResponseDTO/ProjectResponseDTO.cs
@@ -6,6 +6,7 @@ public class ProjectResponseDTO
 {
     public int id { get; set; }
     public string Name { get; set; }
+    public decimal TonsOfOxygen { get; set; }
     public decimal Price { get; set; }
     public string Description { get; set; }
     public Image? Image { get; set; }
